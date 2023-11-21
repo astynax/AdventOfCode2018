1605df77182a23090fb6ec27dab37ca0b0798abe
astynax <astynax@users.noreply.github.com>
2023-11-21 18:10:12 +0400

    Rewrite day01/step2 in a functional manner.

--- src/main/scala/me/astynax/aoc2018/Day01.scala
+++ src/main/scala/me/astynax/aoc2018/Day01.scala
@@ -1,22 +1,20 @@
 package me.astynax.aoc2018
 
+import scala.annotation.tailrec
+
 object Day01 {
 
   def step1(changes: List[Int]): Int = changes.sum
 
   def step2(changes: List[Int]): Int = {
-    val seen = scala.collection.mutable.Set[Int]()
-    var sum = 0
-    while (true) {
-      changes.foreach { i =>
-        sum += i
-        if (seen contains sum) {
-          return sum
-        }
-        seen.add(sum)
+    @tailrec
+    def go(seen: Set[Int], state: Int, input: List[Int]): Int =
+      if (seen contains state) state
+      else input match {
+        case x :: xs => go(seen + state, state + x, xs)
+        case _ => go(seen, state, changes)
       }
-    }
-    0  // impossible
+    go(Set.empty, 0, changes)
   }
 
   lazy val input: List[Int] =
